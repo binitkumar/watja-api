972dc78c8a792f83bd421519cedea0c8e8bf0ea1
BinitKumar <binit.kumar.hbs@gmail.com>
2012-11-09 22:20:44 +0530

    Updated for get request users

--- app/api/wadja/api.rb
+++ app/api/wadja/api.rb
@@ -3,14 +3,14 @@ require 'grape'
 module Wadja
 
   class API < Grape::API
-  
+    format :json  
     resource :users do
     
       post '/create' do
         response_hash = Hash.new
-        response_hash['meta'= Hash.new
+        response_hash['meta']= Hash.new
         user = User.new(params[:user])
-		if user.save
+		if user.valid?
 		  response_code = '000'
 		  response_hash['data']['userid']= user.id
 		else
@@ -22,63 +22,15 @@ module Wadja
       
       
       get do
-        User.all
+        respnose['data'] = User.all
+		response['meta'] = Hash.new
+        response['meta']['response_code'] = '000'		
       end
       
       get ':id' do
         User.find(params[:id])
       end
-      
-      get ':id/posts' do
-        weblog = Weblog.find(params[:id])
-        weblog.posts
-      end
-      
-      post ':id/posts' do
-        @weblog = Weblog.find(params[:id])
-        @post = Post.new
-        @post.title = params[:title] if params[:title]
-        @post.body = params[:body] if params[:body]
-        @weblog.posts << @post
-        
-        status 201
-        @post
-      end
-      
-      delete ':id/posts' do
-        @weblog = Weblog.find(params[:id])
-        @weblog.posts.clear
-      end
-      
-      post do
-        @weblog = Weblog.new
-        @weblog.title = params[:title] if params[:title]
-        @weblog.description = params[:description] if params[:description]
-        @weblog.save 
-        
-        status 201
-        @weblog
-      end
-      
-      put ':id' do
-        @weblog = Weblog.find(params[:id])
-        @weblog.title = params[:title] if params[:title]
-        @weblog.description = params[:description] if params[:description]
-        @weblog.save
-        
-        @weblog
-      end
-      
-      delete do
-        Weblog.destroy_all()
-      end
-    
-      delete ':id' do
-        Weblog.destroy(params[:id])
-      end
-    
-    end 
-             
+	end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :birth_date, :country_id, :email, :email_verified, :name, :password, :username,:photo_url,:gender,:connection_id
-  validate_presence_of :birth_date, :country_id, :email, :email_verified, :name, :password, :username,:photo_url,:gender,:connection_id
-  validate_uniqueness_of :email, :username
+  validates_presence_of :birth_date, :country_id, :email, :email_verified, :name, :password, :username,:photo_url,:gender,:connection_id
+  validates_uniqueness_of :email, :username
 end
